a87bfc1e3709225fcd4e0b0c3d62bad60906d0b7
Caruy Chen <caruychen@gmail.com>
2023-10-21 17:58:45 +0300

    modify test

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -7,16 +7,12 @@ int main(void)
   scavvie.attack("target");
   scavvie.takeDamage(2);
   scavvie.beRepaired(1);
-  scavvie.takeDamage(2);
-  scavvie.takeDamage(2);
-  scavvie.takeDamage(2);
-  scavvie.takeDamage(2);
-  scavvie.takeDamage(2);
-  scavvie.takeDamage(2);
+  scavvie.takeDamage(100);
   scavvie.attack("target");
   scavvie.beRepaired(2);
 
   ScavTrap scavvie2(scavvie);
+  scavvie2.attack("target");
 
   return (0);
 }
